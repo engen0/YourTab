10a7f058217d90a1ea572606d03bdddf612cf674
zweicoder <zhiwei_chong@mymail.sutd.edu.sg>
2015-08-05 00:06:55 +0800

    Modal CSS and logic building block

--- modal.js
+++ modal.js
@@ -1,5 +1,5 @@
 ;
-(function () {
+(function (m) {
     chrome.runtime.onMessage.addListener(function (req, sender, sendRes) {
         switch (req.action) {
             case 'create-modal':
@@ -12,13 +12,22 @@
         }
     });
 
+    // Probably more elegant to do it via mithril but don't wanna waste more time trying to find out how it works
     function createModal(tabsArr) {
         //TODO check if alr clicked/ modal already exists
-        var wrapperDiv =
-            "<div style='position: fixed; left: 0px; top: 0px; background-color: rgb(255, 255, 255); opacity: 0.5; z-index: 2000; height: 1083px; width: 100%;'>" +
-            "<iframe style='width: 100%; height: 100%;'></iframe>" +
-            "</div>";
-        var dialogDiv = $('<div> </div>');
+        // White Overlay
+        var wrapperDiv = $('<div> </div>').css({
+            "position": "fixed",
+            "left": "0px",
+            "top": "0px",
+            "background-color": "rgb(255, 255, 255)",
+            "opacity": "0.5",
+            "z-index": "2000",
+            "height": "1083px",
+            "width": "100%"
+        });
+
+        var dialogDiv = $('<div id="dialog"> </div>');
         dialogDiv.css({
             "position": "fixed",
             "width": "350px",
@@ -27,23 +36,54 @@
             "background-color": "rgb(255, 255, 255)",
             "z-index": "2001",
             "overflow": "auto",
-            "text-align": "center",
+            "text-align": "left",
             "top": "20%",
             "left": "43%"
         });
-        var checkbox = $('<input>', {
-            type:"checkbox",
-            "checked":"checked"
-        });
+        // Title
+        var title = $('<h3></h3>').text("Select Tabs to Save");
+        dialogDiv.append(title);
+
+        //Each tab
         tabsArr.forEach(function (tab) {
-            dialogDiv.append("<p>" + tab.title + "</p>");
+            tab.selected = true;
+            var row = $('<div id="row"> </div>');
+            row.css({
+                "padding": "1em"
+            });
+            var checkbox = $('<input>', {
+                type: "checkbox",
+                "checked": "checked"
+            });
+            checkbox.click(function () {
+                tab.selected = !tab.selected;
+            });
+            var tabTitle = $("<p> </p>").text(tab.title);
+            tabTitle.css({
+                "display": "inline",
+                "margin-left": "1em"
+            });
+            row.append(checkbox);
+            row.append(tabTitle);
+            dialogDiv.append(row);
         });
 
-        dialogDiv.append("<button> Save </button>");
+        // Buttons
+        var saveButton = $("<button> Save </button>");
+        saveButton.click(function () {
+            console.log(tabsArr);
+            $(dialogDiv).remove();
+            $(wrapperDiv).remove();
+        });
+        saveButton.css({
+            "position": "relative",
+            "left": "30%",
+            "width": "40%"
+        });
+        dialogDiv.append(saveButton);
 
         $(document.body).append(wrapperDiv);
         $(document.body).append(dialogDiv);
 
     }
-
 })();
